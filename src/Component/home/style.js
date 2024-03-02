4ff3e3cfe4d4ae330b24c5eb0fcc64bd36734577
YAHIAOTANI <132293911+YAHIAOTANI@users.noreply.github.com>
2024-03-02 21:40:51 +0300

    Update style.js

--- src/Component/home/style.js
+++ src/Component/home/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 export const HomeSection = styled.div`
 height: 500px;
-background: url('../images/Home/home-bg.jpg');
+background: url('../reactPortfolio/images/Home/home-bg.jpg');
 background-size: cover;
 background-position: center;
 text-align: center;
